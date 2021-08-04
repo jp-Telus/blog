89c483cc888ec1d70d1ce71d464534287d69e37a
james <jpoon88@yahoo.com>
2021-08-03 18:38:10 -0700

    add QR Code generation feature

--- config/app.php
+++ config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Additional provider
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
+
+
     ],
 
     /*
@@ -227,6 +231,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // additional alias
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
 
     ],
 
